1d3dee8290d5c672288a8132ffaf3318a4b89e42
Ben Gamari <bgamari.foss@gmail.com>
2013-08-02 09:47:26 -0400

    Test: Port to new interface

--- Test.hs
+++ Test.hs
@@ -1,7 +1,6 @@
 import Pipes
-import qualified Pipes.Prelude as PP
 import Pipes.Vector
 
 main = do
-    a <- runEffect $ runToVectorP $ (PP.fromList [1..5::Int] >-> toVector) ()
+    a <- run $ runToVectorP $ each [1..5::Int] >-> toVector
     print a
