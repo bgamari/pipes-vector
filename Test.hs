012374e173270739e0ce2e56fa6dff2c7f114eaf
Ben Gamari <bgamari.foss@gmail.com>
2013-08-08 13:05:23 -0400

    Test: Add example for pulling element from Vector into Pipe

--- Test.hs
+++ Test.hs
@@ -3,5 +3,11 @@ import Pipes.Vector
 import qualified Data.Vector as V
 
 main = do
+    -- Extracting elements from a Pipe into a Vector
     a <- run $ runToVectorP $ each [1..5] >-> toVector :: IO (V.Vector Int)
     print a
+
+    -- Pushing elements from a Vector into a Pipe requires only each
+    -- from Pipes
+    run $ for (each $ V.enumFromTo 1 5) (lift . print)
+    
