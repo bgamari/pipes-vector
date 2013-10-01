c7ac551c9a4e8b96170e5c117db5a04b2fad9264
Ben Gamari <bgamari.foss@gmail.com>
2013-10-01 15:27:58 -0400

    Update test for run -> runEffect

--- Test.hs
+++ Test.hs
@@ -4,10 +4,10 @@ import qualified Data.Vector as V
 
 main = do
     -- Extracting elements from a Pipe into a Vector
-    a <- run $ runToVectorP $ each [1..5] >-> toVector :: IO (V.Vector Int)
+    a <- runEffect $ runToVectorP $ each [1..5] >-> toVector :: IO (V.Vector Int)
     print a
 
     -- Pushing elements from a Vector into a Pipe is just a standard
     -- Pipes for-each loop
-    run $ for (each $ V.enumFromTo 1 5) (lift . print)
+    runEffect $ for (each $ V.enumFromTo 1 5) (lift . print)
     
