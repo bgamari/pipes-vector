8653fd21ccca37350ca48822fab00b99e104158e
Ben Gamari <ben@smart-cactus.org>
2015-09-27 15:51:20 +0200

    Clean up test

--- Test.hs
+++ Test.hs
@@ -2,6 +2,7 @@ import Pipes
 import Pipes.Vector
 import qualified Data.Vector as V
 
+main :: IO ()
 main = do
     -- Extracting elements from a Pipe into a Vector
     a <- runEffect $ runToVectorP $ each [1..5] >-> toVector :: IO (V.Vector Int)
@@ -10,4 +11,3 @@ main = do
     -- Pushing elements from a Vector into a Pipe is just a standard
     -- Pipes for-each loop
     runEffect $ for (each $ V.enumFromTo 1 5) (lift . print)
-    
